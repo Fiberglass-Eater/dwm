bb118faa1d6cf8a8136efe011aa7402c0aabad37
dom <dom@the_machine.lan>
2020-05-31 03:44:04 -0400

    Changed the colors for the bar

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -1,26 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "TamzenForPowerline:size=14" };
 static const char dmenufont[]       = "TamzenForPowerline:size=14";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#3d3d3d";
-static const char col_gray3[]       = "#7e7e7e";
-static const char col_gray4[]       = "#bebebe";
+static const char col_gray3[]       = "#a89984";
+static const char col_gray4[]       = "#ebdbb2";
 static const char col_cyan[]        = "#98971a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_cyan  },
 };
 
 /* tagging */
